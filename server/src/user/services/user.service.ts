d44a8d7985d2090514f06c7aec11adaecf7e3bb6
Justin Solo <justinpaulosolo@gmail.com>
2022-11-10 16:40:54 -0800

    User controller,  services, tests

--- server/src/user/services/user.service.ts
+++ server/src/user/services/user.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { User, Prisma } from '@prisma/client';
+import { PrismaService } from '../../utils/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async user(id: number): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: { id },
+    });
+  }
+
+  async users(): Promise<User[]> {
+    return this.prisma.user.findMany();
+  }
+
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prisma.user.create({ data });
+  }
+
+  async update(id: number, data: Prisma.UserCreateInput): Promise<User> {
+    return this.prisma.user.update({
+      data: data,
+      where: { id },
+    });
+  }
+
+  async delete(id: number): Promise<{ deleted: boolean; message?: string }> {
+    try {
+      await this.prisma.user.delete({ where: { id } });
+      return { deleted: true };
+    } catch (error) {
+      return { deleted: false, message: error.message };
+    }
+  }
+}
--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './services/user.service';
 import { UserController } from './controllers/user.controller';
+import { PrismaModule } from 'src/utils/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [UserService],
   controllers: [UserController],
 })
